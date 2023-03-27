c482b3a87e9722e31454f9be0d2aaa0ed717bebf
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-27 19:46:32 +0200

    132-től 147-ig véletlen számokat generál

---
+++ 044. veletlenszamokszazharminckettotolszaznegyvenhetig/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _044.veletlenszamokszazharminckettotolszaznegyvenhetig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                Random random = new Random();
+
+                for (int i = 132; i < 148; i++)     // Ez a sor 16 db random számot fog kiírni, mert a 132 és a 147 közti intervallumot veszi, ami 16.
+                {
+                    Console.WriteLine(random.Next(132, 147));   // Ha a zárójelbe nem írunk paramétereket, akkor az integer (int) értékéig, vagyis 2 milliárdig generálja a véletlen   számokat, de a for ciklusban megadott paraméterek miatt akkor is csak 16 db számot fog kiírni. Viszont nekünk nem kellenek ekkora számok, mert a feladat azt mondja, hogy 132-től 147-ig generáljon számokat, így ide is be kell írni a kért paramétereket.
+                }
+
+                //int num = random.Next(1, 7);     // 1-től 7-ig egész számokat generál
+
+                //double num = random.NextDouble();   //tizedes törteket generál
+
+                //Console.WriteLine(num);
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
